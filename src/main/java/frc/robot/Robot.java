0156828f569195a12b319c2121587287b70c051c
ChezyName <mineboy2468@gmail.com>
2022-01-18 20:37:55 -0600

    New Project + Bug Fixes
    Update v0.1
    1/18/22 8:37

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -3,92 +3,101 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
-//import edu.wpi.first.wpilibj.Joystick;
-//import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+
 import edu.wpi.first.wpilibj.TimedRobot;
-//import edu.wpi.first.wpilibj.Timer;
-//import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.math.util.Units;
-//import edu.wpi.first.math.*;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-//import java.io.Console;
-
-//import javax.naming.spi.DirStateFactory.Result;
-
-import org.photonvision.PhotonCamera;
-//import org.photonvision.PhotonUtils;
-
-//import com.ctre.phoenix.*;
+import com.ctre.phoenix.*;
 import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import org.photonvision.*;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the manifest file in the resource
- * directory.
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
  */
 public class Robot extends TimedRobot {
-  private TalonSRX srx = new TalonSRX(25);
-  //private final CameraInfo c_Info = new CameraInfo();
+  private Command m_autonomousCommand;
   private PhotonCamera pc = new PhotonCamera("gloworm");
+  private TalonSRX srx = new TalonSRX(25);
 
+  private RobotContainer m_robotContainer;
+
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
   @Override
   public void robotInit() {
-
+    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    // autonomous chooser on the dashboard.
+    m_robotContainer = new RobotContainer();
   }
 
-  /** This function is run once each time the robot enters autonomous mode. */
+  /**
+   * This function is called every robot packet, no matter the mode. Use this for items like
+   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+    // commands, running already-scheduled commands, removing finished or interrupted commands,
+    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // block in order for anything in the Command-based framework to work.
+    CommandScheduler.getInstance().run();
+  }
+
+  /** This function is called once each time the robot enters Disabled mode. */
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    // schedule the autonomous command (example)
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
+    }
   }
 
   /** This function is called periodically during autonomous. */
   @Override
-  public void autonomousPeriodic() {
-    // yaw > 0 = Turn Robot Right : Left Wheel -1 , Right Wheel +1
-    // yaw < 0 = Turn Robot Left : Left Wheen +1 , Right Wheel -1
-  }
+  public void autonomousPeriodic() {}
 
-  /** This function is called once each time the robot enters teleoperated mode. */
   @Override
-  public void teleopInit() {}
-
-  /** This function is called periodically during teleoperated mode. */
-  @Override
-  public void teleopPeriodic() {
-    // check if target exists and turn robot
-    var result = pc.getLatestResult();
-
-    if(result.hasTargets()){
-      // move wheel
-      srx.set(TalonSRXControlMode.PercentOutput,0.1);
+  public void teleopInit() {
+    var results = pc.getLatestResult();
+    if(results.hasTargets()){
+      srx.set(TalonSRXControlMode.PercentOutput, 0.1);
     }
     else{
-      srx.set(TalonSRXControlMode.PercentOutput,0);
+      srx.set(TalonSRXControlMode.PercentOutput, 0);
     }
   }
 
-  /** This function is called once each time the robot enters test mode. */
+  /** This function is called periodically during operator control. */
   @Override
-  public void testInit() {}
+  public void teleopPeriodic() {}
+
+  @Override
+  public void testInit() {
+    // Cancels all running commands at the start of test mode.
+    CommandScheduler.getInstance().cancelAll();
+  }
 
   /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {}
-
-  public class CameraInfo{
-    // How high the robot's camera is at
-    final double CAMERA_HEIGHT_METERS = Units.inchesToMeters(24);
-    // Avarage Height Of Target 
-    final double TARGET_HEIGHT_METERS = Units.feetToMeters(5);
-    // Angle between horizontal and the camera.
-    final double CAMERA_PITCH_RADIANS = Units.degreesToRadians(0);
-
-    final int MainCamera = 0;
-    final int BlueCamera = 1;
-    final int RedCamera = 2;
-  }
-
-  
 }

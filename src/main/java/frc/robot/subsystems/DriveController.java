913ddba09bdd08c0196d225cc6e8722a8f3509a5
ChezyName <mineboy2468@gmail.com>
2022-01-21 19:30:34 -0600

    Added Comments

--- src/main/java/frc/robot/subsystems/DriveController.java
+++ src/main/java/frc/robot/subsystems/DriveController.java
@@ -2,6 +2,12 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+/*
+  Basic Subsystem Made For 2022 Robot Code
+  Basic Driving And Turning Made For Robot Vision
+*/
+
+// Importing all the Packages
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
@@ -9,14 +15,16 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.*;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+// DriveController Class extending Subsystem to be called in main robotcode
 public class DriveController extends SubsystemBase {
-  /** Creates a new DriveController. */
+  // Creates a new DriveController.
   CANSparkMax frontL;
   CANSparkMax frontR;
   CANSparkMax backL;
   CANSparkMax backR;
-  private final DifferentialDrive robotDrive;
 
+  // differentialDrive to make it control 4 wheels simultaniusly
+  private final DifferentialDrive robotDrive;
 
   public DriveController(int fl, int fr, int bl, int br) {
     frontL = new CANSparkMax(fl,MotorType.kBrushless);
@@ -24,19 +32,16 @@ public class DriveController extends SubsystemBase {
     backL = new CANSparkMax(bl,MotorType.kBrushless);
     backR = new CANSparkMax(br,MotorType.kBrushless);
 
+    //make back wheels follow front wheels
     backL.follow(frontL);
     backR.follow(backR);
 
-
+    // create a drifferentialDrive and give it our CANSparkMax's
     robotDrive = new DifferentialDrive(frontL, frontR);
   }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
-
   public void Drive(double speed,double rotation){
+    //Drive the robot using Speed & Rotation
     robotDrive.arcadeDrive(speed,rotation);
   }
 }
